d7c4a247f5563e473d447f9bf94dead7c2ee1d30
Jon Wood <jon.wood@demandlogic.co.uk>
2023-09-29 20:49:14 +0000

    Provide more detailed documentation

--- src/api_docs_example.py
+++ src/api_docs_example.py
@@ -1,29 +1,37 @@
-from fastapi import FastAPI
-from fastapi.openapi.utils import get_openapi
+from enum import Enum
 
-API_VERSION = "1.0"
+from fastapi import FastAPI
+
+
+class Tags(Enum):
+    """A single reference point for defined tags."""
+
+    greetings = "greetings"
+
 
 # Disable docs so they're not publicly exposed.
-app = FastAPI(docs_url=None, redoc_url=None)
+app = FastAPI(
+    title="Example API",
+    summary="Provides a simple API to greet people, or entire planets.",
+    version="1.0",
+)
 
 
-@app.get("/")
+@app.get("/", summary="Greet the world in general.", tags=[Tags.greetings])
 def index() -> str:
-    """Return the index page."""
+    """Provide a greeting to the entire world, free of prejudice."""
     return "Hello, world!"
 
 
-@app.get("/hello/{name}")
+@app.get("/hello/{name}", summary="Greet a specific person.", tags=[Tags.greetings])
 def hello(name: str) -> str:
-    """Say hello."""
+    """Return a greeting addressed to the person in the URL.
+
+    - **name**: The forename to address the user by.
+    """
     return f"Hello, {name}"
 
 
 def build_docs() -> str:
     """Output the OpenAPI schema."""
-    return get_openapi(
-        title="Hello World",
-        version=API_VERSION,
-        summary="A simple API for saying hello.",
-        routes=app.routes,
-    )
+    return app.openapi()
